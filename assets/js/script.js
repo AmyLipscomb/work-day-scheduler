b4a54c1d7b464f623c2cfc7c959523f9584bbc29
Amy Lipscomb <amylipscomb@amys-mbp.myfiosgateway.com>
2022-10-10 11:08:26 -0400

    added localStorage for the remaining hours

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,4 +8,12 @@ $(".saveBtn").on("click",function(){
     localStorage.setItem(id,txt);
 });
 
-$("#9 .description").val(localStorage.getItem("9")) //Have to do this for every hour. The only thing that will change will be the # and what's in the "" in the localStorage
+$("#9 .description").val(localStorage.getItem("9")); //Have to do this for every hour. The only thing that will change will be the # and what's in the "" in the localStorage
+$("#10 .description").val(localStorage.getItem("10"));
+$("#11 .description").val(localStorage.getItem("11"));
+$("#12 .description").val(localStorage.getItem("12"));
+$("#13 .description").val(localStorage.getItem("13"));
+$("#14 .description").val(localStorage.getItem("14"));
+$("#15 .description").val(localStorage.getItem("15"));
+$("#16 .description").val(localStorage.getItem("16"));
+$("#17 .description").val(localStorage.getItem("17"));
